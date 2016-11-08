97e36f332af58e5f20eb4c74c3acecbb8765c489
Zimbo0397 <zimbo0397@gmail.com>
2016-11-08 15:27:27 +0200

    first changes

--- src/js/main.js
+++ src/js/main.js
@@ -20,3 +20,29 @@ $(document).on('ready', function () {
 		});
 
 });
+
+$('#search-btn').on('click', function() {
+	$('#input-holder').toggleClass('open');
+})
+
+
+$(window).on('load', function() {
+	$('.collections-grid').each(function() {
+		$(this).masonry({itemSelector: '.collections-item'});
+	})
+})
+
+$('.menu-item').each(function() {
+	$(this).on('click', function(e) {
+		e.preventDefault();
+		var dataGroup = "#" + $(this).attr('data-group');
+		$('.collections-grid').removeClass('open');
+		$(dataGroup).addClass('open');
+		$('.collections-grid').each(function() {
+			$(this).trigger('resize')
+               .masonry( 'reloadItems' )
+               .masonry( 'layout');
+		})
+	})
+})
+$('.bx-slider').bxSlider();
